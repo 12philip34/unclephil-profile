e4a55d7fc8e3df13173f9918c5d6b7a9c70eadc9
Nssienphilip <95418182+12philip34@users.noreply.github.com>
2022-11-02 12:17:53 +0100

    updated Introduction component

--- src/components/Animatedbar/index.jsx
+++ src/components/Animatedbar/index.jsx
@@ -3,24 +3,24 @@ import style from "./style.module.css";
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 const AnimatedBar = ({ setIsOpen }) => {
-    const cancleButton = {
+    const cancelButton = {
         color: '#fff',
         fontSize: '50px',
         cursor: 'pointer'
-    }
+    };
 
     return (
         <div>
             <div className={style.dimentionNavbarContainer}>
                 <button className={style.cancelBtn} onClick={() => setIsOpen(false)}>
-                    <CloseRoundedIcon style={cancleButton} />
+                    <CloseRoundedIcon style={cancelButton} />
                 </button>
                 <div className={style.dimentionNavbar}>
                     <ul>
                         <li style={{ zIndex: "5" }}><a href="#home">Home</a></li>
-                        <li style={{ zIndex: "4" }}><a href="#services">Services</a></li>
-                        <li style={{ zIndex: "3" }}><a href="#portfolio">Sites</a></li>
-                        <li style={{ zIndex: "2" }}><a href="#about">about</a></li>
+                        <li style={{ zIndex: "4" }}><a href="#about">about</a></li>
+                        <li style={{ zIndex: "3" }}><a href="#services">Services</a></li>
+                        <li style={{ zIndex: "2" }}><a href="#portfolio">Sites</a></li>
                         <li style={{ zIndex: "1" }} className={style.lastLi}><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
